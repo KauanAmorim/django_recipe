52370a7ec8c6db47b1618407f0582a8265be4270
KauanAmorim <kauanamorim88@gmail.com>
2022-09-11 19:20:50 -0300

    create django recipe

---
+++ recipe/views.py
@@ -0,0 +1,8 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def recipe(request):
+    return render(request, 'recipe.html')
