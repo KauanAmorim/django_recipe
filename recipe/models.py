35c39e2c399adce7e386feee3fcfed597c960ca2
Kauan Amorim da Silva <kauanamorim88@gmail.com>
2022-09-12 20:00:09 -0300

    creating recipe model and create comments to execute when I arrive home

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Recipe(models.model):
+    recipe_name = models.CharField(max_length=200)
+    ingredients = models.TextField()
+    how_to_make = models.TextField()
+    time_to_prepare = models.IntegerField()
+    qtd_yield = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    date_recipe = models.DateTimeField(default=datetime.now, black=True)
+
+# depois será necessário usar os comandos 
+# - python3 manage.py makemigration
+# - python3 manage.py migrate
